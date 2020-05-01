42b08861faabaf0dda4cbc3d6c34643bd7b235bf
Chandler Pope-Lewis <c.popelewis@gmail.com>
2020-05-01 13:10:31 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BitmapEncoding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.encoderButton = new System.Windows.Forms.Button();
+            this.decoderButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.runBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.conversionRate = new System.Windows.Forms.Label();
+            this.outputSize = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.inputSize = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(12, 46);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(257, 567);
+            this.inputBox.TabIndex = 0;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(718, 46);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(257, 567);
+            this.outputBox.TabIndex = 1;
+            // 
+            // encoderButton
+            // 
+            this.encoderButton.Location = new System.Drawing.Point(276, 46);
+            this.encoderButton.Name = "encoderButton";
+            this.encoderButton.Size = new System.Drawing.Size(436, 23);
+            this.encoderButton.TabIndex = 2;
+            this.encoderButton.Text = "Encode";
+            this.encoderButton.UseVisualStyleBackColor = true;
+            this.encoderButton.Click += new System.EventHandler(this.encoderButton_Click);
+            // 
+            // decoderButton
+            // 
+            this.decoderButton.Location = new System.Drawing.Point(276, 75);
+            this.decoderButton.Name = "decoderButton";
+            this.decoderButton.Size = new System.Drawing.Size(436, 23);
+            this.decoderButton.TabIndex = 3;
+            this.decoderButton.Text = "Decode";
+            this.decoderButton.UseVisualStyleBackColor = true;
+            this.decoderButton.Click += new System.EventHandler(this.decoderButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(714, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output:";
+            // 
+            // runBox
+            // 
+            this.runBox.Location = new System.Drawing.Point(296, 218);
+            this.runBox.Multiline = true;
+            this.runBox.Name = "runBox";
+            this.runBox.Size = new System.Drawing.Size(391, 395);
+            this.runBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(292, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Whole runs (debugging):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(292, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Conversion Rate: ";
+            // 
+            // conversionRate
+            // 
+            this.conversionRate.AutoSize = true;
+            this.conversionRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conversionRate.Location = new System.Drawing.Point(433, 163);
+            this.conversionRate.Name = "conversionRate";
+            this.conversionRate.Size = new System.Drawing.Size(0, 20);
+            this.conversionRate.TabIndex = 9;
+            // 
+            // outputSize
+            // 
+            this.outputSize.AutoSize = true;
+            this.outputSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputSize.Location = new System.Drawing.Point(433, 132);
+            this.outputSize.Name = "outputSize";
+            this.outputSize.Size = new System.Drawing.Size(0, 20);
+            this.outputSize.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(292, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Output Size:";
+            // 
+            // inputSize
+            // 
+            this.inputSize.AutoSize = true;
+            this.inputSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputSize.Location = new System.Drawing.Point(433, 101);
+            this.inputSize.Name = "inputSize";
+            this.inputSize.Size = new System.Drawing.Size(0, 20);
+            this.inputSize.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(292, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Input Size:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 625);
+            this.Controls.Add(this.inputSize);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.outputSize);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.conversionRate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.runBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decoderButton);
+            this.Controls.Add(this.encoderButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button encoderButton;
+        private System.Windows.Forms.Button decoderButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox runBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label conversionRate;
+        private System.Windows.Forms.Label outputSize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label inputSize;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapEncoding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string encodingRunLengths(string stringCode, out Queue<int> code)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            char start = stringCode.First();
+
+            sb.Append(stringCode.First());
+            sb.Append(", ");
+
+            code = new Queue<int>();
+            code.Enqueue(start.Equals('0') ? 0 : 1);
+            Queue<char> input = new Queue<char>(stringCode.ToCharArray());
+
+            int currRun = 0;
+            while(input.Count > 0)
+            {
+                char current = input.Dequeue();
+
+                if (start.Equals(current))
+                    currRun++;
+                else
+                {
+                    code.Enqueue(currRun);
+                    sb.Append(currRun);
+                    sb.Append(", ");
+                    currRun = 1;
+                    start = current;
+                }
+            }
+            code.Enqueue(currRun);
+            sb.Append(currRun);
+
+            return sb.ToString();
+        }
+
+        private string Encoder(Queue<int> input)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(input.Dequeue());
+
+            while(input.Count > 0)
+            {
+                string binary = Convert.ToString(input.Dequeue(), 2);
+                sb.Append(string.Concat(Enumerable.Repeat("0", binary.Length - 1)));
+                sb.Append(binary);
+            }
+
+            return sb.ToString();
+        }
+
+        private string Decoder(string stringInput)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Queue<char> input = new Queue<char>(stringInput.ToCharArray());
+            int current = input.Dequeue().Equals('0') ? 0 : 1;
+
+            while (input.Count > 0)
+            {
+                int run = 0;
+                while (input.Peek().Equals('0'))
+                {
+                    run++;
+                    input.Dequeue();
+                }
+                Console.WriteLine(input.Count);
+
+                StringBuilder binarySB = new StringBuilder();
+                for (int i = 0; i < run + 1; i++) binarySB.Append(input.Dequeue());
+                int length = Convert.ToInt32(binarySB.ToString(), 2);
+                Console.WriteLine(length);
+                for (int i = 0; i < length; i++) sb.Append(current);
+                current = current == 0 ? 1 : 0;
+            }
+
+            return sb.ToString();
+        }
+
+        private void encoderButton_Click(object sender, EventArgs e)
+        {
+            runBox.Text = encodingRunLengths(inputBox.Text, out Queue<int> runlengths);
+            outputBox.Text = Encoder(runlengths);
+
+            inputSize.Text = inputBox.Text.Length.ToString();
+            outputSize.Text = outputBox.Text.Length.ToString();
+            conversionRate.Text = (((outputBox.Text.Length * 1.0) / inputBox.Text.Length) * 100.0).ToString();
+        }
+
+        private void decoderButton_Click(object sender, EventArgs e)
+        {
+            runBox.Text = "";
+            outputBox.Text = Decoder(inputBox.Text);
+        }
+    }
+}
